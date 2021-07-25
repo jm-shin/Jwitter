b699188d920fd31dab94882c64b825205a9dcb4a
jongminshin <ace4463@gmail.com>
2021-07-25 20:29:26 +0900

    add tweet testcode

--- tests/tweet.test.js
+++ tests/tweet.test.js
@@ -1,3 +1,18 @@
-import tweets from '../data/tweet';
+//모카로 변경예정.
 
-describe(test('should ', () => {}));
+const tweetRepository = require('../data/tweet.js');
+
+describe(
+  test('tweet 등록', () => {
+    const text = '트윗 내용입니다.';
+    const name = '이름';
+    const username = '유저네임';
+    const tweet = tweetRepository.create(text, name, username);
+    const result = {
+      text,
+      name,
+      username
+    };
+    expect(tweet).toContain(result);
+  })
+);
