669b437c5adb119ac5f0643195c747a8367fb60b
jongminshin <ace4463@gmail.com>
2021-08-08 18:51:56 +0900

    add mongoose

--- data/auth.js
+++ data/auth.js
@@ -1,21 +1,46 @@
-import { db } from '../db/database.js';
+// import { db } from '../db/database.js';
+import Mongoose from 'mongoose';
+import { useVirtualId } from '../database/database2.js';
+
+const userSchema = new Mongoose.Schema({
+  username: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  url: String,
+});
+
+useVirtualId(userSchema);
+const User = Mongoose.model('User', userSchema);
 
 export async function findByUsername(username) {
-  return db
-    .execute('SELECT * FROM users WHERE username=?', [username])
-    .then((result) => result[0][0]);
+  return User.findOne({ username });
 }
 
 export async function findById(id) {
-  return db
-    .execute('SELECT * FROM users WHERE id=?', [id])
-    .then((result) => result[0][0]);
+  return User.findById(id);
 }
 
 export async function createUser(user) {
-  const { username, password, name, email, url } = user;
-  db.execute(
-    'INSERT INTO (username, password, name, email, url) VALUSES (?, ?, ?, ?, ?)',
-    [username, password, name, email, url]
-  ).then((result) => result[0].insertId);
+  return new User(user).save().then((data) => data.id);
 }
+
+// export async function findByUsername(username) {
+//   return db
+//     .execute('SELECT * FROM users WHERE username=?', [username])
+//     .then((result) => result[0][0]);
+// }
+//
+// export async function findById(id) {
+//   return db
+//     .execute('SELECT * FROM users WHERE id=?', [id])
+//     .then((result) => result[0][0]);
+// }
+//
+// export async function createUser(user) {
+//   const { username, password, name, email, url } = user;
+//   db.execute(
+//     'INSERT INTO (username, password, name, email, url) VALUSES (?, ?, ?, ?, ?)',
+//     [username, password, name, email, url]
+//   ).then((result) => result[0].insertId);
+// }
--- db/database2.js
+++ db/database2.js
@@ -1,9 +1,24 @@
-import MongoDB from 'mongodb';
-import {config} from '../config.js'
+// import MongoDB from 'mongodb';
+import Mongoose from 'mongoose';
+import { config } from '../config.js'
 
 export function connectMongoDB() {
-    return MongoDB.MongoClient.connect(config.mongo.host, {
+    return Mongoose.connect(config.mongo.host, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        useFindAndModify: false,
     });
+}
+
+export function useVirtualId(schema) {
+    schema.virtual('id').get(function () {
+        return this._id.toString();
+    });
+    schema.set('toJSON', { virtuals: true });
+    schema.set('toOject', { virtuals: true });
+}
+
+let db;
+export function getTweets() {
+    return db.collection('tweets');
 }
